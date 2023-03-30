1c0556ce3f1f4051282ddd78e9abdef9f261d60e
maricamola <maricamola98@libero.it>
2023-03-30 18:29:04 +0200

    added array with 16 bombs

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const level = document.querySelector('#level');
 //Creo un array per i veri livelli
 const levels = [100, 81, 49];
 
+//Genero variabile con array vuota per le bombe:
+const numBombs = 16;
+let bombs = [] ;
+const celNumbers = 100;
+
+//PLAY
 playButton.addEventListener('click', play);
 
 
@@ -14,12 +20,10 @@ function play(){
 }
 
 
-
 //Creo il ciclo per generare i 100 square
-for(let i = 1; i <= 100; i++ ){
+for(let i = 1; i <= celNumbers; i++ ){
   
   const firstSquare =  createSquare();
-  
   //aggiungiamo un evento al 'click' 
   // 'this' ci serve per richiamare l'elemento che abbiamo     cliccato
   firstSquare.addEventListener('click', function(){
@@ -30,12 +34,35 @@ for(let i = 1; i <= 100; i++ ){
 
 /**********funzioni*************/
 
+//Funzione Bombs
+
+bombs = generateBombs(celNumbers)
+
+function generateBombs(celNumbers){
+
+  let bombs = [] ;
+
+  while(bombs.length < numBombs){
+
+    const bomb = getRandomNumber(1, celNumbers);
+
+    if(!bombs.includes(bomb)) bombs.push(bomb);
+  }
+  console.log(bombs)
+  return bombs;
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 //Creo funzione per generare lo square
 
 function createSquare() {
-  const newSquare = document.createElement('div');
+    const newSquare = document.createElement('div');
    //assegno nome alla classe square
-  newSquare.className = 'square';
+    newSquare.className = 'square';
+
    //Aggiungo un evento per vedere il numero della casella selezionata in console.log
 
    //IndexOf restituisce un valore numerico che rappresenta la posizione dell'elemento nella stringa
@@ -48,10 +75,12 @@ function createSquare() {
     
     //Aggiungo al newsquare nell'HTML il numero
     newSquare.innerHTML= `<span> ${squareNumber}</span> `
+    console.log(`Hai selezionato la casella numero ${squareNumber}`);
   });
-  
+
   return newSquare;
 }
+  
 
 
 //Creo funzione per il bottone che dovrà mostrare il container
